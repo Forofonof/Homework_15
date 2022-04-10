f579b3d7724234b02527353774ee497a630b0ec7
Forofonof <DanielResult1@outlook.com>
2022-04-10 19:32:20 +0300

    Добавьте файлы проекта.

---
+++ Homework_15/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+    internal class Program
+{
+    static void Main(string[] args)
+    {
+        Random random = new Random();
+
+        int limitMinimum = 1;
+        int limitMaximum = 27;
+        int numberMininum = 100;
+        int numberMaxinum = 999;
+        int number;
+        int count = 0;
+
+        number = random.Next(limitMinimum, limitMaximum);
+
+        for (int i = number; i <= numberMaxinum; i += number)
+        {
+            if (i > numberMininum)
+            {
+                count++;
+                Console.Write($"{i}, ");
+            }
+        }
+        Console.WriteLine($"\nКоличество натуральных чисел - {count}. \nЧисло N - {number}.");
+    }
+}
